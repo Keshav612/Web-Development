ff159bbb984de407222ef705cf09d06b4ef520eb
Keshav612 <500062098@stu.upes.ac.in>
2021-01-22 15:06:20 +0530

    Hello World by java Script

--- JavaScript/demo.js
+++ JavaScript/demo.js
@@ -1,3 +1,1 @@
-<script type="text/javascript">
-            document.write("Hello World");  //document represents entire document..write is a function which prints the message.
-</script>
+document.write("Hello World");  
